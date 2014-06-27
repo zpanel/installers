3a6d3ac091dfaf0a82cd026dfe9e32141344f2ac
Kimpe Andy <andykimpe@gmail.com>
2014-06-27 17:12:43 +0200

    Update httpd24.sql

--- upgrade/beta/CentOS_6/httpd24.sql
+++ upgrade/beta/CentOS_6/httpd24.sql
@@ -1,1 +1,2 @@
- 
+UPDATE `zpanel_core`.`x_settings` SET `so_value_tx` = 'httpd24' WHERE `x_settings`.`so_id_pk` = 101;
+UPDATE `zpanel_core`.`x_settings` SET `so_value_tx` = 'httpd24-httpd' WHERE `x_settings`.`so_id_pk` = 102;
